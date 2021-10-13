1faee0e762a65ff4ef34b0ed8b7dd1562716568e
Alexander Alexeev <alexax66@gmail.com>
2021-10-13 22:20:54 +0300

    remove redundant dbt's tool

--- build_kernel_freebsd.sh
+++ build_kernel_freebsd.sh
@@ -8,15 +8,8 @@ export ARCH
 
 THREAD=-j$(bc <<< $(sysctl -n hw.ncpu)+2)
 
-DTBH_PLATFORM_CODE=0x50a6
-DTBH_SUBTYPE_CODE=0x217584da
-
 gmake -C $(pwd) O=output ARCH=$ARCH "lineageos_a3xelte-octa_defconfig"
 gmake $THREAD -C $(pwd) O=output ARCH=$ARCH
 
 cp output/arch/arm64/boot/Image  output/arch/arm64/boot/boot.img-kernel
-
-$(pwd)/dtbTool  -o "$(pwd)/output/arch/arm64/boot/dt.img" -s 2048 -d "$(pwd)/output/arch/arm64/boot/dts/" --platform $DTBH_PLATFORM_CODE --subtype $DTBH_SUBTYPE_CODE
 cp output/arch/arm64/boot/dt.img  output/arch/arm64/boot/boot.img-dt
-
-#24 2>&1 | tee -a  log.txt
